b8fe777f71c0a06769fc181524d188daabe5de0e
B Araújo <tiz.araujo96@gmail.com>
2020-05-16 00:36:22 +0100

    Converted jQuery to pure javascript.

--- js/lane_slide.js
+++ js/lane_slide.js
@@ -1,56 +1,58 @@
-var laneWrap = $("#lane-wrap");
-var lane = $(".lane"),
-    $lane1 = $("#lane1"),
-    $lane2 = $("#lane2");
+var laneWrap = document.getElementById("lane-wrap");
+var lane = document.getElementsByClassName("lane"),
+    $lane1 = document.getElementById("lane1"),
+    $lane2 = document.getElementById("lane2");
+
+var lane1ImgHolder = document.getElementById("lane1").getElementsByClassName("imgHolder");
+var lane2ImgHolder = document.getElementById("lane2").getElementsByClassName("imgHolder");
 
 function init(){
-    $( "#lane1 div.imgHolder" ).each(function() {
+    for(var i = 0 ; i<lane1ImgHolder.length; i++) {
         var x = 150;
         var y = Math.random() * (100 - (-20)) + (-20);
-        
-        $(this).css({
-            "top":y+"px",
-            "margin-left":x+"px"
-        });
-    });
+
+        lane1ImgHolder[i].style.top = y+"px";
+        lane1ImgHolder[i].style.marginLeft = x+"px";
+    }
     
-    $( "#lane2 div.imgHolder" ).each(function() {
+    for(var i = 0; i<lane2ImgHolder.length; i++){
         //pick two random numbers between 0 and 100 for x, etc
         var x = 16;
         var y = Math.random() * (-100);
     
-        $(this).css({
-            "bottom":y+"px",
-            "margin-left":x+"px"
-        });
-    });
+       lane2ImgHolder[i].style.bottom = y+"px";
+       lane2ImgHolder[i].style.marginLeft = x+"px";
+    }
 }
-
 init();
 //lane 1
-var lane1ImgHolder = $("#lane1 div.imgHolder");
-    lane1Img0 = lane1ImgHolder.eq(0).clone(true),
-    lane1Img1c1 = lane1ImgHolder.eq(1).clone(true),
-    lane1Img1c2 = lane1ImgHolder.eq(1).clone(true),
-    lane1Img2c1 = lane1ImgHolder.eq(2).clone(true);
-    lane1Img2c2 = lane1ImgHolder.eq(2).clone(true);
+var lane1Img0 = lane1ImgHolder[0].cloneNode(true),
+    lane1Img1c1 = lane1ImgHolder[1].cloneNode(true),
+    lane1Img1c2 = lane1ImgHolder[1].cloneNode(true),
+    lane1Img2c1 = lane1ImgHolder[2].cloneNode(true),
+    lane1Img2c2 = lane1ImgHolder[2].cloneNode(true);
 
-$("#lane1 div.imgHolder").eq(0).before(lane1Img2c2);    
-$("#lane1 div.imgHolder").eq(0).before(lane1Img2c1);
-$("#lane1 div.imgHolder").eq(0).before(lane1Img1c1);
+$lane1.insertBefore(lane1Img2c2, lane1ImgHolder[0]);
+$lane1.insertBefore(lane1Img2c1, lane1ImgHolder[0]);
+$lane1.insertBefore(lane1Img1c1, lane1ImgHolder[0]);
 
-$lane1.append(lane1Img0);
-$lane1.append(lane1Img1c2);
-$lane1.append(lane1Img2c2);
+$lane1.appendChild(lane1Img0);
+$lane1.appendChild(lane1Img1c2);
+$lane1.appendChild(lane1Img2c2);
 
-var sliderStartForward = $("#lane1 div.imgHolder").eq(0).position().left;
-var sliderEndForward = $("#lane1 div.imgHolder").eq(6).position().left -$lane1.width();
-var sliderStartBackward = $lane1.width() - $("#lane1 div.imgHolder").eq(0).position().left;
+function getCssProperty(elmId, property){
+    var elem = document.getElementById(elmId);
+    return window.getComputedStyle(elem,null).getPropertyValue(property);
+ }
 
-$("#lane1 div.imgHolder").eq(0).addClass("first");
+var sliderStartForward = lane1ImgHolder[0].style.left;
+var sliderEndForward = lane1ImgHolder[6].style.left - $lane1.offsetWidth;
+var sliderStartBackward = $lane1.offsetWidth - lane1ImgHolder[0].style.left;
 
-$lane1.scrollLeft( sliderStartForward );
-$lane1.on('scroll', function(){
+lane1ImgHolder[0].classList.add("first");
+
+//$lane1.scrollLeft( sliderStartForward );
+/*$lane1.on('scroll', function(){
     if($lane1.scrollLeft() < 1){
         $lane1.scrollLeft(sliderStartBackward);
     }
@@ -58,33 +60,32 @@ $lane1.on('scroll', function(){
     else if($lane1.scrollLeft() > sliderEndForward){
         $lane1.scrollLeft(sliderStartForward);
     }
-});
+});*/
 
 //lane 2
-var lane2ImgHolder = $("#lane2 div.imgHolder");
-    lane2Img0 = lane2ImgHolder.eq(0).clone(true),
-    lane2Img1c1 = lane2ImgHolder.eq(1).clone(true),
-    lane2Img1c2 = lane2ImgHolder.eq(1).clone(true),
-    lane2Img2c1 = lane2ImgHolder.eq(2).clone(true);
-    lane2Img2c2 = lane2ImgHolder.eq(2).clone(true);
-    lane2Img1 = lane2ImgHolder.eq(3).clone(true);
+var lane2Img0 = lane2ImgHolder[0].cloneNode(true),
+    lane2Img1c1 = lane2ImgHolder[1].cloneNode(true),
+    lane2Img1c2 = lane2ImgHolder[1].cloneNode(true),
+    lane2Img2c1 = lane2ImgHolder[2].cloneNode(true),
+    lane2Img2c2 = lane2ImgHolder[2].cloneNode(true),
+    lane2Img1 = lane2ImgHolder[3].cloneNode(true);
  
-$("#lane2 div.imgHolder").eq(0).before(lane2Img1);
-$("#lane2 div.imgHolder").eq(0).before(lane2Img2c1);
-$("#lane2 div.imgHolder").eq(0).before(lane2Img1c1);
+$lane2.insertBefore(lane2Img1, lane2ImgHolder[0]);
+$lane2.insertBefore(lane2Img2c1, lane2ImgHolder[0]);
+$lane2.insertBefore(lane2Img1c1, lane2ImgHolder[0]);
 
-$lane2.append(lane2Img0);
-$lane2.append(lane2Img1c2);
-$lane2.append(lane2Img2c2);
+$lane2.appendChild(lane2Img0);
+$lane2.appendChild(lane2Img1c2);
+$lane2.appendChild(lane2Img2c2);
 
-var sliderStartForward2 = $("#lane2 div.imgHolder").eq(0).position().left;
-var sliderEndForward2 = $("#lane2 div.imgHolder").eq(6).position().left -$lane2.width();
-var sliderStartBackward2 = $lane2.width() - $("#lane2 div.imgHolder").eq(0).position().left;
+var sliderStartForward2 = lane2ImgHolder[0].style.left;
+var sliderEndForward2 = lane2ImgHolder[6].style.left -$lane2.offsetWidth;
+var sliderStartBackward2 = $lane2.offsetWidth - lane2ImgHolder[0].style.left;
 
-$("#lane2 div.imgHolder").eq(0).addClass("first2");
+lane2ImgHolder[0].classList.add("first2");
 
-$lane2.scrollLeft( sliderStartForward2 );
-$lane2.on('scroll', function(){
+//$lane2.scrollLeft( sliderStartForward2 );
+/*$lane2.on('scroll', function(){
 
     if(sliderEndForward2 > 1){
         $lane2.scrollLeft(sliderStartBackward2);
@@ -93,4 +94,4 @@ $lane2.on('scroll', function(){
     else if($lane2.scrollLeft() < sliderEndForward2){
         $lane2.scrollLeft(sliderStartForward2);
     }
-});
+});*/
